db03638e04cd21c412e633d3f79db93d8c22c027
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2021-11-22 19:18:24 +0530

    remove email password authentication inactive code and used globally styled buttons

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,59 +5,15 @@ import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-nativ
 import { auth } from '../firebase'
 import { firebaseConfig } from '../firebaseConfig'
 import * as firebase from 'firebase'
+import colors from '../constants/colors';
+import PrimaryButton from '../components/PrimaryButton'
+import SecondaryButton from '../components/SecondaryButton';
 
 const LoginScreen = () => {
 
-    // const [email, setEmail] = useState('')
-    // const [password, setPassword] = useState('')
     const [phone, setPhone] = useState()
     const [otp, setOtp] = useState()
     const [verificationId, setVerificationId] = useState();
-
-    {/* FOR EMAIL AND PASSWORD BASED AUTHENTICATION */ }
-
-
-    // const handleUserSignUp = () => {
-    //     auth
-    //         .createUserWithEmailAndPassword(email, password)
-    //         .then(userCredentials => {
-    //             const user = userCredentials.user
-    //             console.log(user.email)
-    //             // User id
-    //             console.log(user.uid)
-    //             return (firestore.collection('users').doc(user.uid).set({
-    //                 // Add the fields that you want to add to a user document inside users collection
-
-    //             }))
-    //         })
-    //         .catch(error => alert(error))
-    // }
-
-    // const handleLogin = () => {
-    //     auth
-    //         .signInWithEmailAndPassword(email, password)
-    //         .then(userCredentials => {
-    //             const user = userCredentials.user
-    //             console.log("Logged in with: ", user.email)
-    //         })
-    //         .catch(error => alert(error))
-    // }
-
-    // const handleResetPassword = () => {
-    //     auth
-    //         .sendPasswordResetEmail(email)
-    //         .then(() => {
-    //             alert("Password reset email sent!")
-    //         })
-    //         .catch((error) => {
-    //             alert(error)
-    //         })
-    // }
-
-    // const ref_input_password = useRef()
-
-    {/* ------------------------------------------ */ }
-
 
     const handleSendOTP = async () => {
         try {
@@ -123,66 +79,11 @@ const LoginScreen = () => {
                     keyboardType="number-pad"
                     maxLength={6}
                 />
-
-                {/* FOR EMAIL AND PASSWORD BASED AUTHENTICATION */}
-                {/* <TextInput
-                    autoCapitalize="none"
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={v => setEmail(v)}
-                    style={styles.input}
-                    autoComplete="email"
-                    returnKeyType="next"
-                    onSubmitEditing={() => { ref_input_password.current.focus() }}
-                    blurOnSubmit={false}
-                />
-                <TextInput
-                    ref={ref_input_password}
-                    placeholder="Password"
-                    value={password}
-                    onChangeText={v => setPassword(v)}
-                    style={styles.input}
-                    secureTextEntry
-                    onSubmitEditing={handleLogin}
-                /> */}
-                {/* ------------------------------------------ */}
-
             </View>
 
             <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    onPress={handleSendOTP}
-                    style={styles.button}
-                >
-                    <Text style={styles.buttonText}>Send OTP</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleVerifyOTP}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Verify</Text>
-                </TouchableOpacity>
-
-                {/* FOR EMAIL AND PASSWORD BASED AUTHENTICATION */}
-                {/* <TouchableOpacity
-                    onPress={handleLogin}
-                    style={styles.button}
-                >
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleUserSignUp}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Register</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleResetPassword}
-                >
-                    <Text>Forgot Password</Text>
-                </TouchableOpacity> */}
-                {/* ------------------------------------------ */}
-
+                <PrimaryButton onPress={handleSendOTP} text="Send OTP" style={{ marginBottom: 10 }} />
+                <SecondaryButton text="Verify" onPress={handleVerifyOTP} />
             </View>
         </View>
     )
@@ -212,28 +113,4 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: 20
     },
-    button: {
-        backgroundColor: '#EA7C69',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-        alignSelf: 'center'
-    },
-    buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: '#EA7C69',
-        borderWidth: 2
-    },
-    buttonOutlineText: {
-        color: '#EA7C69',
-        fontWeight: '700',
-        fontSize: 16,
-        alignSelf: 'center'
-    }
 })
